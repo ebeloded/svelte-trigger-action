3def7afb66d8164758b7ec0f941b64410bd7916c
Evgenij (Eugene) Beloded <belodedevgenij@gmail.com>
2022-01-18 15:13:30 +0700

    Add missing plugins in config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 const name = 'svelte-trigger-action'
 
 export default defineConfig({
+  plugins: [svelte()],
   build: {
     lib: {
       entry: new URL('src/lib/index.ts', import.meta.url).pathname,

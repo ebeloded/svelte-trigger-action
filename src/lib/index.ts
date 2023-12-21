5290aaea80bc2b49b10f4101aaf35d7630e18769
Evgenij (Eugene) Beloded <belodedevgenij@gmail.com>
2023-12-21 12:32:06 -0800

    Transition to kit package

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,63 +1,55 @@
-type TriggerCallback<T = any> = (triggerParam: T) => void
+type TriggerCallback<T = any> = (triggerParam?: T) => void;
 const createCallbacksTrigger = <T>() => {
-  let callbacks: TriggerCallback<T>[] = []
+	let callbacks: TriggerCallback<T>[] = [];
 
-  return {
-    registerCallback(cb: TriggerCallback<T>) {
-      callbacks.push(cb)
-      return () => (callbacks = [])
-    },
-    trigger(triggerParam: T): Promise<any> {
-      return Promise.all(
-        callbacks.map((cb) => Promise.resolve(cb(triggerParam)))
-      )
-    },
-  }
-}
+	return {
+		registerCallback(cb: TriggerCallback<T>) {
+			callbacks.push(cb);
+			return () => (callbacks = []);
+		},
+		trigger(triggerParam?: T): Promise<any> {
+			return Promise.all(callbacks.map((cb) => Promise.resolve(cb(triggerParam))));
+		}
+	};
+};
 
 export const createTriggerAction = <P = any, T = any | undefined>(
-  fn: (node: HTMLElement, param: P, triggerParam: T) => void | Promise<void>
+	fn: (node: HTMLElement, param: P, triggerParam?: T) => void | Promise<void>
 ) => {
-  const { trigger, registerCallback } = createCallbacksTrigger<T>()
-  return {
-    triggerAction: (node: HTMLElement, initialParam: P) => {
-      let param = initialParam
-      const cleanup = registerCallback((triggerParam: T) =>
-        fn(node, param, triggerParam)
-      )
-      return {
-        update(param: P) {
-          param = param
-        },
-        destroy() {
-          cleanup()
-        },
-      }
-    },
-    trigger,
-  }
-}
+	const { trigger, registerCallback } = createCallbacksTrigger<T>();
+	return {
+		triggerAction: (node: HTMLElement, initialParam: P) => {
+			let param = initialParam;
+			const cleanup = registerCallback((triggerParam?: T) => fn(node, param, triggerParam));
+			return {
+				update(param: P) {
+					param = param;
+				},
+				destroy() {
+					cleanup();
+				}
+			};
+		},
+		trigger
+	};
+};
 
 export const createAnimationTriggerAction = (globalClassName?: string) => {
-  const { trigger, triggerAction } = createTriggerAction<
-    string | undefined,
-    string | undefined
-  >(
-    (node, actionClassName, triggerClassName) =>
-      new Promise((resolve) => {
-        let className = triggerClassName || actionClassName || globalClassName
-        if (className) {
-          node.addEventListener('animationend', () => {
-            node.classList.remove(className!)
-            resolve()
-          })
-          node.classList.add(className)
-        }
-      })
-  )
-  return {
-    triggerAnimation: (className?: string) => trigger(className),
-    animationAction: (node: HTMLElement, className?: string) =>
-      triggerAction(node, className),
-  }
-}
+	const { trigger, triggerAction } = createTriggerAction<string | undefined, string | undefined>(
+		(node, actionClassName, triggerClassName) =>
+			new Promise((resolve) => {
+				let className = triggerClassName || actionClassName || globalClassName;
+				if (className) {
+					node.addEventListener('animationend', () => {
+						node.classList.remove(className!);
+						resolve();
+					});
+					node.classList.add(className);
+				}
+			})
+	);
+	return {
+		triggerAnimation: (className?: string) => trigger(className),
+		animationAction: (node: HTMLElement, className?: string) => triggerAction(node, className)
+	};
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+	import { onMount } from 'svelte';
+
+	import { createAnimationTriggerAction, createTriggerAction } from '$lib';
+
+	const { triggerAnimation, animationAction } = createAnimationTriggerAction();
+	const { trigger, triggerAction } = createTriggerAction(async (node, param, triggerParam) => {
+		console.log('action callback', node, param, triggerParam);
+	});
+
+	onMount(() => {
+		trigger('test');
+	});
+
+	async function animate() {
+		console.log('start shaking');
+		await triggerAnimation('shake');
+		console.log('stop shaking');
+	}
+</script>
+
+<button use:animationAction on:click={animate}> shake me </button>
+
+<div use:triggerAction={'hello'}></div>
+
+<style>
+	:global(.shake) {
+		animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+		transform: translate3d(0, 0, 0);
+		backface-visibility: hidden;
+		perspective: 1000px;
+	}
+
+	@keyframes shake {
+		10%,
+		90% {
+			transform: translate3d(-1px, 0, 0);
+		}
+
+		20%,
+		80% {
+			transform: translate3d(2px, 0, 0);
+		}
+
+		30%,
+		50%,
+		70% {
+			transform: translate3d(-4px, 0, 0);
+		}
+
+		40%,
+		60% {
+			transform: translate3d(4px, 0, 0);
+		}
+	}
+</style>
